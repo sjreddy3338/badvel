6731f297869865b334f8958d0a95cab469c3423b
sjreddy3338 <sjreddy3338@gmail.com>
2019-12-06 12:24:19 +0530

    badvel project

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, Badvel");
+    System.out.println("Hello. mydkur");
+    System.out.println("Hello. pulivendula");
   }
 }
